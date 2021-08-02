05ce763818df75153c5694e681b2e035cc31d526
mgeffroy <79372976+mgeffroy@users.noreply.github.com>
2021-08-02 18:19:02 -0500

    Created map

---
+++ static/js/logic.js
@@ -0,0 +1,24 @@
+// Create map object  
+var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 4,
+});
+// Create title layer 
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/outdoors-v11",
+  accessToken: API_KEY
+}).addTo(myMap)
+
+// Get GeoJSON information 
+
+
+//Create markers for earthquakes 
+
+
+
